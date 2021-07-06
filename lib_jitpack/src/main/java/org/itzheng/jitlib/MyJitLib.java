0dac1c00cd032c0848f5b5642f58261a4305d684
zhengyd2 <zhengyd2@motorola.com>
2021-07-06 17:08:04 +0800

    apply plugin: 'com.github.dcendents.android-maven' test note

--- lib_jitpack/src/main/java/org/itzheng/jitlib/MyJitLib.java
+++ lib_jitpack/src/main/java/org/itzheng/jitlib/MyJitLib.java
@@ -1,9 +1,17 @@
 package org.itzheng.jitlib;
 
+/**
+ * Test Jit
+ */
 public class MyJitLib {
     /**
      * this is welcome String.
      */
     public static final String WELCOME = "Hello,It Jit Lib.";
     private static final String V_05 = "android-maven-gradle-plugin";
+    /**
+     * 添加 classpath 'com.github.dcendents:android-maven-gradle-plugin:1.5'
+     * apply plugin: 'com.github.dcendents.android-maven'
+     */
+    private static final String V_0_6 = "apply plugin: 'com.github.dcendents.android-maven'";
 }
